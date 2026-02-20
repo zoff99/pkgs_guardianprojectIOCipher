1f585a119f9efeae81cee417abb0729726adec51
zoff99 <zoff99@users.noreply.github.com>
2026-02-20 17:12:16 +0100

    create and add checksum file

--- tools/jitpackio_prepare_new_release_version.sh
+++ tools/jitpackio_prepare_new_release_version.sh
@@ -33,6 +33,21 @@ echo $next_m_version
 sed -i -e 's#unzip local_maven_iocipher_.*$#unzip local_maven_iocipher_'"$next_m_version"'.zip#' "$f1"
 sed -i -e 's#.version..*./version.#<version>'"$next_m_version"'</version>#' "$f2"
 
+# generate checksum file and add and commit it
+cd "$basedir"/
+checksum_file="pkgs_guardianprojectIOCipher-""$next_m_version"".aar.sha256"
+tmp_dir="unpack_temp_$$"
+rm -Rf "$tmp_dir"/
+mkdir -p "$tmp_dir"/
+cd "$tmp_dir"/ && unzip ../local_maven_iocipher_"$next_m_version".zip
+cd ./.m2/repository/info/guardianproject/iocipher/IOCipher2/"$next_m_version"/
+sha256sum IOCipher2-"$next_m_version".aar
+sha256sum IOCipher2-"$next_m_version".aar > "$basedir"/"$checksum_file"
+cd "$basedir"/
+rm -Rf "$tmp_dir"/
+
+git add "$basedir"/"$checksum_file"
+
 commit_message="$next_m_version"
 tag_name="$next_m_version"
 
